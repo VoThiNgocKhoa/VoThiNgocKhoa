88dc1f73eacade9991c0447bb947e9cbd7844c7f
VoThiNgocKhoa <vongockhoa310104@gmail.com>
2024-09-15 02:31:27 +0700

    Update keobuabao.py

--- keobuabao.py
+++ keobuabao.py
@@ -6,18 +6,18 @@ Created on Sun Aug 18 16:19:39 2024
 """
 
 import random
-chon=["keo", "bua ", "bao"]
-you=input("nhap lua chon cua ban (keo,bua,bao)")
-computer=random.choice(chon)
+lua_chon=["keo", "bua ", "bao"]
+you=input("nhap lua chon cua ban (keo,bua,bao): ")
+computer=random.choice(lua_chon)
 print("May : ",computer)
 if you not in chon:
     print("lua chon cua ban khong hop le .") 
 else:
     if you==computer:
         print("hoa ")
-    elif (you == "keo " and computer == "bao") or \
-         (you == "bua " and computer == "keo") or \
-         (you == "bao " and computer == "bua"):
+    elif (you == "keo" and computer == "bao") or \
+         (you == "bua" and computer == "keo") or \
+         (you == "bao" and computer == "bua"):
         print("You win")
     else:
         print("Computer win")
